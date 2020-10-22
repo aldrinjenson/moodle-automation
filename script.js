8af2a64f9156b16a3913d282e274e5b11d42a93a
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-22 13:43:31 +0530

    Add Save and Verify
    - Write script for saving attendance
    - Add verification for ensuring that Present is marked correctly
    - Show log messages to show status

--- script.js
+++ script.js
@@ -6,14 +6,51 @@ const links = require("./links");
 
 const markAttendance = async (page) => {
   try {
-    for (const link of Object.values(links)) {
-      await page.goto(link);
-      await page.waitForSelector(".statuscol");
-      const statusLink = await page.evaluate(() => {
-        // return [...document.querySelectorAll(".statuscol a")][0];
-        return [...document.querySelectorAll(".statuscol a")];
-      });
+    for (const [subject, link] of Object.entries(links)) {
+      await page.goto(link, { waitUntil: "networkidle2" });
+      // await page.waitForSelector(".statuscol");
+      // const statusLink = await page.evaluate(() => {
+      //   return [...document.querySelectorAll(".statuscol a")];
+      // });
+      const statusLink = await page.$eval(".statuscol a", (el) => el);
       console.log(statusLink);
+
+      if (statusLink && statusLink.length) {
+        await page.goto(statusLink, { waitUntil: "networkidle2" });
+
+        const attendanceLink = await page.$eval(
+          ".statuscol.cell a",
+          (e) => e.href
+        );
+        if (attendanceLink) {
+          await page.goto(attendanceLink, { waitUntil: "networkidle2" });
+          await page.waitForSelector(".statusdesc");
+          await page.evaluate(() => {
+            let label = [...document.querySelectorAll(".statusdesc")].find(
+              (el) => el.innerText === "Present"
+            );
+            label.click();
+            return;
+          });
+          await page.click("#id_submitbutton");
+
+          await page.waitForSelector("#user-notifications");
+          const notificationText = await page.$eval(
+            "#user-notifications div ",
+            (e) => e.innerText
+          );
+          if (
+            notificationText.slice(2) ===
+            "Your attendance in this session has been recorded."
+          ) {
+            console.log(`Attendance marked for ${subject}`);
+          } else {
+            console.log(`Error in marking attendance for ${subject}`);
+          }
+        }
+      } else {
+        console.log(`Not available for ${subject}`);
+      }
     }
   } catch (error) {
     logger.error(error);
@@ -27,12 +64,13 @@ const scrape = async () => {
 
   await page.type("#username", process.env.USERNAME);
   await page.type("#password", process.env.PASS);
-  // await page.keyboard.press("Enter");
+  await page.keyboard.press("Enter");
 
   await page.waitForSelector(".colatt");
 
   await markAttendance(page);
-  await browser.waitForTarget(() => {});
+
+  await browser.waitForTarget(() => false);
   // await browser.close();
 };
 
