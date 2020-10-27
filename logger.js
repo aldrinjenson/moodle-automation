4a089492bf38803a8203b25ee1e54ffb78d645cc
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-27 17:16:12 +0530

    Add logger fn

--- logger.js
+++ logger.js
@@ -1,1 +1,12 @@
-const logger = (msg, type) => {};
+const fs = require("fs");
+const logStream = fs.createWriteStream("logs.txt", { flags: "a" });
+
+const logger = (msg) => {
+  console.log(msg);
+  if (typeof msg !== String) {
+    msg = JSON.stringify(msg);
+  }
+  logStream.write(msg + "\n");
+};
+
+module.exports = logger;
