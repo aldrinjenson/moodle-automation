240d73d275a4a4f141263f098b7d9a0014ff5381
Aldrin Jenson <aldrinjenson@gmail.com>
2020-11-05 20:50:38 +0530

    Update Subject links

--- subjectLinks.js
+++ subjectLinks.js
@@ -3,9 +3,9 @@ const subjectLinks = {
   OopLab: "http://moodle.mec.ac.in/mod/attendance/view.php?id=576",
   Ds: "http://moodle.mec.ac.in/mod/attendance/view.php?id=590",
   Lsd: "http://moodle.mec.ac.in/mod/attendance/view.php?id=550",
-  DMS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=371",
+  DMS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=3364",
   SustainableEngineering:
-    "http://moodle.mec.ac.in/mod/attendance/view.php?id=2239",
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=3894",
   EcEMinor: "http://moodle.mec.ac.in/mod/attendance/view.php?id=948",
 };
 
