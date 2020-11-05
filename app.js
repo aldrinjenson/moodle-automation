2aa2abe39862fe9f783f664e71979e064c5cb0a8
Aldrin Jenson <aldrinjenson@gmail.com>
2020-11-05 19:57:20 +0530

    Add both telegram and web service

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-// const {
-//   main,
-//   subjectsMarked,
-//   manuallyMarked,
-//   timesMarked,
-//   timesChecked,
-//   scrape,
-// } = require("./script");
-// const express = require("express");
-// const subjectLinks = require("./subjectLinks");
-
-// const PORT = process.env.PORT || 5000;
-// const app = express();
-// app.set("view engine", "ejs");
-// app.use(express.static("public"));
-
-// const subjectsLeftToMark = Object.keys(subjectLinks).filter(
-//   (subject) =>
-//     !subjectsMarked.includes(subject) && !manuallyMarked.includes(subject)
-// );
-
-// app.get("/", (req, res) => {
-//   res.render("index", {
-//     main,
-//     subjectLinks,
-//     subjectsMarked,
-//     manuallyMarked,
-//     timesMarked,
-//     timesChecked,
-//     subjectsLeftToMark,
-//   });
-// });
-
-// app.get("/scrape", (req, res) => {
-//   scrape();
-//   res.redirect("/");
-// });
-
-// main();
-// app.listen(PORT);
+const {
+  main,
+  subjectsMarked,
+  manuallyMarked,
+  timesMarked,
+  timesChecked,
+  scrape,
+} = require("./script");
+const express = require("express");
+const subjectLinks = require("./subjectLinks");
 
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
-const { scrape, main } = require("./script");
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.onText(/\/scrape/, () => {
   bot.sendMessage(process.env.CHAT_ID, "Scraping..");
   scrape(bot);
 });
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const subjectsLeftToMark = Object.keys(subjectLinks).filter(
+  (subject) =>
+    !subjectsMarked.includes(subject) && !manuallyMarked.includes(subject)
+);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    main,
+    subjectLinks,
+    subjectsMarked,
+    manuallyMarked,
+    timesMarked,
+    timesChecked,
+    subjectsLeftToMark,
+  });
+});
+
+app.get("/scrape", (req, res) => {
+  scrape();
+  res.redirect("/");
+});
+app.listen(PORT);
 
 main(bot);
