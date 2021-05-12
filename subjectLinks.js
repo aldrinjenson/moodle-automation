d9a046e0f30ea70f098710f21aa05ac5b733f4f1
Aldrin Jenson <aldrinjenson@gmail.com>
2021-05-12 12:18:43 +0530

    Update subject links

--- subjectLinks.js
+++ subjectLinks.js
@@ -1,15 +1,13 @@
 const subjectLinks = {
-  "Ds Lab": "http://moodle.mec.ac.in/mod/attendance/view.php?id=231",
-  "Oop Lab": "http://moodle.mec.ac.in/mod/attendance/view.php?id=576",
-  DSA: "http://moodle.mec.ac.in/mod/attendance/view.php?id=590",
-  LSD: "http://moodle.mec.ac.in/mod/attendance/view.php?id=550",
-  DMS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=371",
-  "EcE Minor": "http://moodle.mec.ac.in/mod/attendance/view.php?id=948",
-  "Design & Engineering":
-    "http://moodle.mec.ac.in/mod/attendance/view.php?id=345",
+  "OS Lab": "http://moodle.mec.ac.in/mod/attendance/view.php?id=10854",
+  DBMS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10120",
+  COA: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10118",
+  OS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10137",
+  "Machine Learning - Honors":
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=11073",
+  "Graph Theory": "http://moodle.mec.ac.in/mod/attendance/view.php?id=10468",
+  "Professional Ethics":
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=10380",
 };
 
-// OOP: "http://moodle.mec.ac.in/course/view.php?id=131",
-// "Sustainable Engineering":
-//   "http://moodle.mec.ac.in/mod/attendance/view.php?id=3894",
 module.exports = subjectLinks;
