c9c1153c609430c703315ecde7c791be2d239143
Aldrin Jenson <aldrinjenson@gmail.com>
2021-12-17 10:06:08 +0530

    updated subjectLinks

--- subjectLinks.js
+++ subjectLinks.js
@@ -1,13 +1,7 @@
 const subjectLinks = {
-  "OS Lab": "http://moodle.mec.ac.in/mod/attendance/view.php?id=10854",
-  DBMS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10120",
-  COA: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10118",
-  OS: "http://moodle.mec.ac.in/mod/attendance/view.php?id=10137",
-  "Machine Learning - Honors":
-    "http://moodle.mec.ac.in/mod/attendance/view.php?id=11073",
-  "Graph Theory": "http://moodle.mec.ac.in/mod/attendance/view.php?id=10468",
-  "Professional Ethics":
-    "http://moodle.mec.ac.in/mod/attendance/view.php?id=10380",
+  MoSS: "https://moodle.mec.ac.in/course/view.php?id=648",
+  "Microprocessor and SSLAB": "https://moodle.mec.ac.in/course/view.php?id=684",
+  "DBMS Lab": "https://moodle.mec.ac.in/course/view.php?id=645",
 };
 
 module.exports = subjectLinks;
